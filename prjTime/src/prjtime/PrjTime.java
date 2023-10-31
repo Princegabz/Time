53d9a7a60cbb270e6824d0d9a8b054ab0e0b3a69
Gabriel <83117210+Princegabz@users.noreply.github.com>
2023-10-31 18:01:45 +0200

    Add files via upload

---
+++ prjTime/src/prjtime/PrjTime.java
@@ -0,0 +1,15 @@
+
+package prjtime;
+
+public class PrjTime {
+    
+  
+    public static void main(String[] args) {
+    int minutes = 197;
+    int hours = 197/60;
+    int mins = 197% 60;
+    System.out.println(minutes+"minutes becomes"+hours+"hours and"+mins+"mins");
+        
+    }
+    
+}
